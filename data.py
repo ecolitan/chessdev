2537f91dda3079d68ac1ea97df1ebb493e359074
Aaron Cossey <cossey@aaron.glamus.de>
2011-12-16 14:49:15 +0100

    1

--- data.py
+++ data.py
@@ -1,3 +1,26 @@
-emptyBoard = [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None]
-startBoard = ['r',  'n',  'b',  'q',  'k',  'b',  'n',  'r', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', None,  None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P',  'R',  'N',  'B',  'Q',  'K',  'B',  'N',  'R']
-piecetypes = ['r',  'n',  'b',  'q',  'k',  'b',  'n',  'r', 'p',  'P',  'R',  'N',  'B',  'Q',  'K',  'B',  'N',  'R']
+emptyBoard = ([None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None], 'w', [None,None,None,None],None,0,0)
+startBoard = (['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', None,  None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'r',  'n',  'b',  'q',  'k',  'b',  'n',  'r'],'w',['K','Q','k','q'],None,0,0)
+startwithoutrooks = ([None, 'N', 'B', 'Q', 'K', 'B', 'N', None, 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', None,  None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', None,  'n',  'b',  'q',  'k',  'b',  'n',  None],'w',['K','Q','k','q'],None,0,0)
+piecetypes = ['R', 'N', 'B', 'Q', 'K', 'P', 'r', 'n', 'b', 'q', 'k', 'p']
+
+"""
+a8 b8 c8 d8 e8 f8 g8 h8
+a7 b7 c7 d7 e7 f7 g7 h7
+a6 b6 c6 d6 e6 f6 g6 h6
+a5 b5 c5 d5 e5 f5 g5 h5
+a4 b4 c4 d4 e4 f4 g4 h4
+a3 b3 c3 d3 e3 f3 g3 h3
+a2 b2 c2 d2 e2 f2 g2 h2
+a1 b1 c1 d1 h1 f1 g1 h1
+
+
+57 58 59 60 61 62 63 64
+49 50 51 52 53 54 55 56
+41 42 43 44 45 46 47 48
+33 34 35 36 37 38 39 40
+25 26 27 28 29 30 31 32
+17 18 19 20 21 22 23 24
+09 10 11 12 13 14 15 16
+01 02 03 04 05 06 07 08
+"""
+
--- main.py
+++ main.py
@@ -6,30 +6,39 @@ from collections import Counter
 class BoardPosition:
     """A single board position.
     Position format: ( [ a1, b1, ... , h1, a2, ... , h8 ] ,          # Piece placement or None,
-                       [ w | b ],                                    # Side to move.
+                       'w' | 'b',                                    # Side to move.
                        [ 'K', 'Q', 'k', 'q' ],                       # Castling rights or None.
-                       int,                                          # en passant square ot None.
+                       int,                                          # en passant square or None.
                        int,                                          # half move clock - since last pawn advance or capture.
                        int,                                          # fullmove number.
                     )"""
                             
     def __init__(self,  position):
         self.position = position
+        self.pieceplacement = position[0]
+        self.sidetomove = position[1]
+        self.castlingrights = position[2]
+        self.epsquare = position[3]
+        self.halfmoveclock = position[4]
+        self.fullmoves = position[5]
     
     def MaterialCount(self):
         """Returns a tuple with a dictionary of the counts of each piece, and total number of pieces"""
         piececount = Counter()
-        for piece in self.position:
-            piececount[piece] += 1
-        piecetotal = (sum(piececount.values()) - 32)
-        return (dict(piececount),  piecetotal)
+        for piece in self.pieceplacement:
+            if piece:
+                piececount[piece] += 1
+        piecetotal = sum(piececount.values())
+        return ((dict(piececount),  piecetotal))
     
     def IsLegal(self):
-        """Tests if the Position is Legal."""
+        """Returns True|False for position legality."""
         return True
 
 emptytest = BoardPosition(emptyBoard)
 starttest = BoardPosition(startBoard)
+rooktest = BoardPosition(startwithoutrooks)
 
 print emptytest.MaterialCount()
 print starttest.MaterialCount()
+print rooktest.MaterialCount()
