de90c4ce9e9308344edbf27016fe3b6b8cfe2e9c
Aaron Cossey <cossey@aaron.glamus.de>
2011-12-20 10:44:15 +0100

    compiles clean again.

--- chessdev/__init__.py
+++ chessdev/__init__.py
@@ -1,7 +1,6 @@
-from chessdev.boardposition import BoardPosition
 from chessdev.boardrelations import BoardRelations
 
 __author__ = "Aaron Cossey"
 __version__ = "0.0.1"
 
-__all__ = ["boardposition", "boardrelations"]
+__all__ = ["boardrelations"]
--- test/quicktest.py
+++ test/quicktest.py
@@ -1,29 +1,20 @@
-from chessdev.boardposition import BoardPosition
 from chessdev.boardrelations import BoardRelations
 from chessdev.kingmoves import KingMoves
 from chessdev.data.data import *
 from chessdev.data.examples import *
 
 def quicktest():
-
-        
-    def testBoardRelations():
-        """Tests for BoardRelations Objects."""
-        emptytest = BoardRelations(emptyBoard[0])
-        starttest = BoardRelations(startBoard[0])
-        rooktest = BoardRelations(startBoardnorooks[0])
-        return True
         
     # Tests for KingMoves 
-    starttest = BoardPosition(startBoard)   # startposition
-    test1 = BoardPosition(testpos1)         # no w queen
-    test2 = BoardPosition(testpos2)         # king on e6 in check
-    test3 = BoardPosition(testpos3)         # No white king
-    test4 = BoardPosition(testpos4)         # No kings
+    starttest = BoardRelations(startBoard)   # startposition
+    test1 = BoardRelations(testpos1)         # no w queen
+    test2 = BoardRelations(testpos2)         # king on e6 in check
+    test3 = BoardRelations(testpos3)         # No white king
+    test4 = BoardRelations(testpos4)         # No kings
     
     print KingMoves(starttest).kingsquare()
     print KingMoves(test1).kingsquare()
     print KingMoves(test2).kingsquare()
     print KingMoves(test3).kingsquare()
     
-    testBoardRelations()
+
