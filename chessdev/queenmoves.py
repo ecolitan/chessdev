c006f67553d7e17dea568594fd196df1bceb13d3
Aaron Cossey <cossey@aaron.glamus.de>
2011-12-20 15:29:06 +0100

    Add new Class QueenMoves

---
+++ chessdev/queenmoves.py
@@ -0,0 +1,16 @@
+from chessdev.data.data import *
+
+class QueenMoves:
+    """Calculates legal queen moves.
+    Accepts a BoardRelations object.
+    """
+    
+    def __init__(self. position):
+        self.position = position
+        self.sidetomove = position.sidetomove
+        
+    def QueenSquare(self):
+        """Returns the square of the queen for the player to more.
+        Return int
+        """
+        return 0
